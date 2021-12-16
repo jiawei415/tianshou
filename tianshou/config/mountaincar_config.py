08092dfe2f86b98975441a73f2e262bc9e14e22c
ztjiaweixu <ztjiaweixu@tencent.com>
2021-12-17 01:14:17 +0800

    update default config

--- tianshou/config/mountaincar_config.py
+++ tianshou/config/mountaincar_config.py
@@ -1,14 +1,13 @@
 alg_config = dict(
-    num_atoms=51,
     v_max=100,
-    noise_dim=32,
-    buffer_size=int(2e5),
+    based_weight_decay=0.0003125,
+    hyper_weight_decay=0.,
     hyper_reg_coef=0.01,
     prior_scale=1.,
     init_type=None,
-    batch_size=320,
+    hidden_sizes=[512,512],
     target_update_freq=100,
-    step_per_collect=10,
+    step_per_collect=2,
 )
 
 '''successful for HyperC51
@@ -18,7 +17,7 @@ alg_config = dict(
         noise_dim=2,
         buffer_size=50000,
         based_weight_decay=0.0003125,
-        hyper_weight_decay=0.0003125,
+        hyper_weight_decay=0,
         hyper_reg_coef=0.01,
         noise_dim=2,
         prior_std=2,

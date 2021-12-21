75849b818ac85f1176a593e22e3f1cc992028d10
ztjiaweixu <ztjiaweixu@tencent.com>
2021-12-21 11:29:56 +0800

    add experiments scripts

---
+++ experiments/ensemble/run_mountaincar_dqn.sh
@@ -0,0 +1,83 @@
+export CUDA_VISIBLE_DEVICES=$1
+seed=$2
+
+## environment config
+task=MountainCar-v0
+max_step=500
+## training config
+same_noise_update=True
+batch_noise_update=True
+target_update_freq=100
+batch_size=128
+lr=0.0001
+weight_decay=0.0003125
+v_max=0
+num_atoms=1
+## algorithm config
+alg_type=ensemble
+ensemble_num=20
+prior_std=1
+prior_scale=1
+## action selection config
+sample_per_step=False
+action_sample_num=1
+action_select_scheme=Greedy
+value_gap_eps=0.001
+value_var_eps=0.001
+## network config
+ensemble_layer=0
+ensemble_size=0
+hidden_layer=2
+hidden_size=512
+use_dueling=True
+init_type=""
+## epoch config
+epoch=1000
+step_per_collect=2
+## buffer config
+buffer_size=200000
+min_buffer_size=${max_step}
+
+## overwrite config
+## W. Prior -- Sample per epiosde -- Dependent noise update
+config01="{'prior_std':1,'sample_per_step':False,'same_noise_update':True,'use_dueling':${use_dueling}}"
+## W. Prior -- Sample per step -- Dependent noise update
+config02="{'prior_std':1,'sample_per_step':True,'same_noise_update':True,'use_dueling':${use_dueling}}"
+## W. Prior -- Sample per epiosde -- Independent noise update
+config03="{'prior_std':1,'sample_per_step':False,'same_noise_update':False,'use_dueling':${use_dueling}}"
+## W. Prior -- Sample per step -- Independent noise update
+config04="{'prior_std':1,'sample_per_step':True,'same_noise_update':False,'use_dueling':${use_dueling}}"
+## W/O. Prior -- Sample per epiosde -- Dependent noise update
+config05="{'prior_std':0,'sample_per_step':False,'same_noise_update':True,'use_dueling':${use_dueling}}"
+## W/O. Prior -- Sample per step -- Dependent noise update
+config06="{'prior_std':0,'sample_per_step':True,'same_noise_update':True,'use_dueling':${use_dueling}}"
+## W/O. Prior -- Sample per epiosde -- Independent noise update
+config07="{'prior_std':0,'sample_per_step':False,'same_noise_update':False,'use_dueling':${use_dueling}}"
+## W/O. Prior -- Sample per step -- Independent noise update
+config08="{'prior_std':0,'sample_per_step':True,'same_noise_update':False,'use_dueling':${use_dueling}}"
+
+config="${config01}"
+
+time=2
+for i in $(seq 5)
+do
+    tag=$(date "+%Y%m%d%H%M%S")
+    python -m tianshou.scripts.run_${alg_type} --seed ${seed} --task ${task} --max-step ${max_step} \
+    --target-update-freq=${target_update_freq} --batch-size=${batch_size} --lr=${lr} \
+    --weight-decay=${weight_decay} --v-max=${v_max} --num-atoms=${num_atoms} \
+    --ensemble-num=${ensemble_num} --prior-std=${prior_std} --prior-scale=${prior_scale} \
+    --ensemble-layer=${ensemble_layer} --ensemble-size=${ensemble_size} \
+    --action-sample-num=${action_sample_num} --action-select-scheme=${action_select_scheme} \
+    --value-gap-eps=${value_gap_eps} --value-var-eps=${value_var_eps} \
+    --hidden-layer=${hidden_layer} --hidden-size=${hidden_size} --init-type=${init_type} \
+    --epoch=${epoch} --step-per-collect=${step_per_collect} \
+    --buffer-size=${buffer_size} --min-buffer-size=${min_buffer_size} \
+    # --logdir='' \
+    --config ${config} \
+    > ~/logs/${alg_type}_${task}_${tag}_3.out 2> ~/logs/${alg_type}_${task}_${tag}_3.err &
+    echo "run $seed $tag"
+    let seed=$seed+1
+    sleep ${time}
+done
+
+# ps -ef | grep MountainCar | awk '{print $2}'| xargs kill -9
---
+++ experiments/run_cpu.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+cmd=$1
+task=$2
+config=$3
+alg=$4
+
+# echo "cmd: $cmd, task: $task"
+
+if [ "$cmd" = 'kill' ]; then
+    if [ -z "$task" ]; then
+        echo "kill tianshou"
+        ps -ef| grep tianshou |awk '{print $2}'|xargs kill -9
+    fi
+    if [ -n "$task" ]; then
+        echo "kill $task"
+        ps -ef| grep $task |awk '{print $2}'|xargs kill -9
+    fi
+fi
+
+if [ "$cmd" = 'run' ]; then
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    __conda_setup="$('/root/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "/root/miniconda3/etc/profile.d/conda.sh" ]; then
+            . "/root/miniconda3/etc/profile.d/conda.sh"
+        else
+            export PATH="/root/miniconda3/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+    # <<< conda initialize <<<
+    conda activate torch
+    
+    python /data/jiawei/Code/tianshou/tianshou/scripts/run_${alg}.py --task $task --config $config --logdir '/data/jiawei/Code/tianshou/results' > log.out 2> log.err &
+    # python /data/jiawei/Code/tianshou/other.py --task $task --config $config > log.out 2> log.err &
+    # echo "run $task"
+fi
